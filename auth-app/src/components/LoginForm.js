de7b79926c126ca34e7b33e7b914887350c3b273
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2024-08-13 21:44:38 -0500

    form css completed

--- auth-app/src/components/LoginForm.js
+++ auth-app/src/components/LoginForm.js
@@ -1,10 +1,44 @@
-import '../styles/loginForm.css';
+import '../styles/LoginForm.css';
 
 export default function Form() {
   return (
-    <form>
-      <h1>Bienvenido</h1>
-      <p>Por favor inicia sesión</p>
+    <form className="login-form">
+
+      <div className="login-form-main-container">
+        <h1 className="title">Bienvenido</h1>
+        <p className="sub-title">Por favor inicia sesión</p>
+
+        <div className="fields-container">
+          <div className="form-element">
+            <label className="form-label">Usuario</label>
+            <input
+              className="value-input"
+              placeholder="ingrese su usuario"
+              type="text"
+              id="username"
+              value=""
+              required
+            />
+          </div>
+
+          <div className="form-element">
+            <label className="form-label">Contraseña</label>
+            <input
+              className="value-input"
+              placeholder="ingrese su contraseña"
+              type="text"
+              id="password"
+              value=""
+              required
+            />
+          </div>
+        </div>
+
+        <button className="forgot-password-btn">Olvidé la contraseña</button>
+
+        <button className="sign-in-btn">Ingresar</button>
+      </div>
+
     </form>
   )
 }
