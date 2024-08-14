ea2636fc990c7785b124ec0b4d46acdd24918f5d
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2024-08-13 23:12:22 -0500

    login view completed

--- auth-app/src/app/App.js
+++ auth-app/src/app/App.js
@@ -1,11 +1,14 @@
+import '../styles/App.css';
 import Form from "../components/LoginForm.js";
 import LogoWelcome from "../components/LoginLogo.js";
 
 function App() {
   return (
     <>
-      <LogoWelcome/>
-      <Form/>
+      <div className="main-cards-container">
+        <LogoWelcome/>
+        <Form/>
+      </div>
     </>
   )
 }
